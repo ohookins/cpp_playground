c6c607b1b609669fca92c3f9cd7341d979f248a3
Oliver Hookins <oliver@contentful.com>
2018-01-16 11:10:49 +0100

    Initial Commit

---
+++ MyCPPPRoject/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  MyCPPPRoject
+//
+//  Created by Oliver Hookins on 16.01.18.
+//  Copyright © 2018 Oliver Hookins. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
