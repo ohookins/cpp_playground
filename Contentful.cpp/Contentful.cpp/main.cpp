9bc8d89452a807ce7a15de59d0eb4849ccff2ad3
Oliver Hookins <ohookins@gmail.com>
2018-03-29 20:50:16 +0200

    Move existing content into subfolder.

--- Contentful.cpp/Contentful.cpp/main.cpp
+++ Contentful.cpp/Contentful.cpp/main.cpp
@@ -10,8 +10,8 @@
 #include "ContentfulClient.hpp"
 
 int main(int argc, const char * argv[]) {
-    auto client = ContentfulClient("796733cd79dbbfcba82941f0896c664f0930d2623d99cd2e39ae4bdc97a1b409");
-    client.GetEntries("m20artr3xszm");
+    auto client = ContentfulClient("API_KEY");
+    client.GetEntries("SPACE_ID");
     
     return 0;
 }
